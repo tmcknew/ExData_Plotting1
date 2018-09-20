25524b821107fa33e75a8cc73d0f510dfd9e7b82
tmcknew <tmcknew@users.noreply.github.com>
2018-09-20 01:04:15 -0700

    finishes plot1.r

--- plot1.R
+++ plot1.R
@@ -1,11 +1,5 @@
-# clear env
-rm(list = ls())
-
-# load data.table for fread
-library(data.table)
-library(dplyr) 
-library(tibble)
-library(lubridate)
+# load data.table for fread, dplyr and tibble for table manipulation
+library(data.table); library(dplyr); library(tibble)
 
 # make sure the data is here
 data_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
@@ -23,15 +17,15 @@ if (!file.exists(data_file)) {
 
 # read the data for 2007-02-01 and 2007-02-02
 # Note that in this dataset missing values are coded as ?.
-
 hpc <- fread("egrep '^Date|^1\\/2\\/2007|^2\\/2\\/2007' household_power_consumption.txt", 
              na.strings="?")
 
 # convert the Date and Time variables to Date/Time classes in R using the 
-# strptime() and as.Date() functions.
+# strptime() and as.POSIXct()
 hpc <- mutate(hpc, datetime = as.POSIXct(strptime(paste(hpc$Date,hpc$Time), 
                                                   format="%d/%m/%Y %T") ) )
 
+# open output file
 png(filename = "plot1.png",
     width = 480, height = 480, units = "px", pointsize = 12)
 
@@ -39,5 +33,6 @@ png(filename = "plot1.png",
 hist(hpc$Global_active_power, 
      col = "red",
      main = "Global Active Power",
-     xlab = "Global Active Power (kilowatts)"
-)
+     xlab = "Global Active Power (kilowatts)")
+
+dev.off()
